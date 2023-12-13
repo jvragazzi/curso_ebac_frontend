614bdfb5a2e1ea3b4f86df65c7ea692eeb75e8c5
João Victor Ragazzi <123113282+jvragazzi@users.noreply.github.com>
2023-12-13 18:54:33 -0300

    Add files via upload

---
+++ exercicio_es6.js
@@ -0,0 +1,16 @@
+    const alunos = [
+        { nome: 'João', nota: 8 },
+        { nome: 'Maria', nota: 5 },
+        { nome: 'Pedro', nota: 7 },
+        { nome: 'Ana', nota: 6 },
+        { nome: 'Carlos', nota: 4 }
+    ];
+    
+    function alunosAprovados(arrayDeAlunos) {
+        return arrayDeAlunos.filter(aluno => aluno.nota >= 6);
+    }
+    
+    const alunosAprovadosArray = alunosAprovados(alunos);
+    
+    console.log(alunosAprovadosArray);
+    
