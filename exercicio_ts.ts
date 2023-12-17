05371110fbc24b8d3c0eeca06b87f6889f1adae0
João Victor Ragazzi <123113282+jvragazzi@users.noreply.github.com>
2023-12-16 22:12:50 -0300

    Add files via upload

---
+++ exercicio_ts.ts
@@ -0,0 +1,9 @@
+    // Função de multiplicação
+    function multiplicar(x: number, y: number): number {
+        return x * y;
+    }
+    
+    // Função de saudação
+    function saudacao(nome: string): string {
+        return "Olá " + nome;
+    }
